f73758e0a9cc735dc4e0b28edf45d7b8b7ad555d
ValeryGnj <valerygnj@gmail.com>
2024-03-04 15:27:27 +0200

    2

--- import requests.py
+++ import requests.py
@@ -1,14 +1,18 @@
 import requests
-payload = {"login": "secret_login", "password":"secret_pass"}
+
+# Отправляем первый запрос для получения куки
+payload = {"login": "secret_login", "password": "secret_pass"}
 response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
 
+# Получаем значение куки
 cookie_value = response1.cookies.get('auth_cookie')
 
-cookies ={}
+# Создаем словарь с куками
+cookies = {}
 if cookie_value is not None:
     cookies.update({'auth_cookie': cookie_value})
 
+# Отправляем второй запрос с использованием куки
 response2 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", cookies=cookies)
 
 print(response2.text)
-
